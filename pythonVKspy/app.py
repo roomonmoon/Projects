2a196b358cb496504e7f354dc3014195151b3002
Sergey Degtyar <68108301+roomonmoon@users.noreply.github.com>
2022-03-16 20:37:04 +0300

    Add files via upload

---
+++ pythonVKspy/app.py
@@ -0,0 +1,14 @@
+from auth_data import token
+import requests
+import json 
+import time 
+import os  # not sure 
+
+def spy(token):
+    print(token)
+
+def main():
+    spy()
+
+if __name__ == '__main__':
+    main()
