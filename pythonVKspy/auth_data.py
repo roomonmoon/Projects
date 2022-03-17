18d7ca430e17981890f187ee3064f0f3bcef38c4
Sergey Degtyar <68108301+roomonmoon@users.noreply.github.com>
2022-03-17 22:56:53 +0300

    Delete auth_data.py

--- pythonVKspy/auth_data.py
+++
@@ -1,4 +0,0 @@
-access_url = f'https://oauth.vk.com/authorize?client_id=8104338&display=page&scope=users&friends&offline&response_type=token&v=5.131&state=123456'
-TOKEN = '8399298403bbac118b3704f6371db8835bd168f1c05922f3d7bff1e50d61486027c25ec35632dafaaae71'
-BOT_TOKEN = '5213597366:AAGzOX_-UkaL9bEqzgYHlQuM3LMLiTfLTBE'
-user_id = '461232958'
