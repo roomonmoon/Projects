f50fdb52d93a580a2d76eb7d223f2e17d0d50b60
Sergey Degtyar <68108301+roomonmoon@users.noreply.github.com>
2022-03-19 19:28:16 +0300

    Delete auth_data.py

--- pythonVKspy/auth_data.py
+++
@@ -1,4 +0,0 @@
-TOKEN = 'a04f70bfa04f70bfa04f70bfd4a034d92daa04fa04f70bfc27e781afea65009cc255cea'
-BOT_TOKEN = '5213597366:AAGzOX_-UkaL9bEqzgYHlQuM3LMLiTfLTBE'
-user_id = '498256285'
-second_id = '461232958'
