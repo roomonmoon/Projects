3c048bf37dca793018782aab18a0055a6375d62c
Sergey Degtyar <degtyar2023@mail.ru>
2022-04-07 18:29:27 +0300

    main

---
+++ htdocs/validation-php/check.php
@@ -0,0 +1,36 @@
+<?php 
+    $login = filter_var(trim($_POST['login']), 
+    FILTER_SANITIZE_STRING);
+    $name = filter_var(trim($_POST['name']), 
+    FILTER_SANITIZE_STRING);
+    $lastname = filter_var(trim($_POST['lastname']), 
+    FILTER_SANITIZE_STRING);
+    $middlename = filter_var(trim($_POST['middlename']), 
+    FILTER_SANITIZE_STRING);
+    $speciality = filter_var(trim($_POST['speciality']), 
+    FILTER_SANITIZE_STRING);
+    $place = filter_var(trim($_POST['place']), 
+    FILTER_SANITIZE_STRING);
+    $pass = filter_var(trim($_POST['pass']), 
+    FILTER_SANITIZE_STRING);
+    $repass = filter_var(trim($_POST['repass']), 
+    FILTER_SANITIZE_STRING);
+    
+    if(mb_strlen($login) < 5 || mb_strlen($login) > 12) {
+        echo "Недопустимая длина логина";
+        exit();
+    } else if(mb_strlen($pass) < 6 || mb_strlen($pass) > 32) {
+        echo "Недопустимая длина пароля (от 6 до 32 символов)";
+        exit();
+    } else if($repass != $pass) {
+        echo "Повторный пароль введён неправильно!";
+        exit();
+    }
+
+    $mysql = new mysqli('localhost','root','','register-db');
+    $mysql->query("INSERT INTO `users` (`login`, `name`, `lastname`, `middlename`, `speciality`, `place`, `pass`)
+    VALUES('$login', '$name', '$lastname', '$middlename', '$speciality', '$place', '$pass')");
+
+    $mysql->close()
+    header('Location: /')
+?>
--- pythonVKspy/app.py
+++ pythonVKspy/app.py
@@ -6,66 +6,71 @@ import requests
 import json
 import time
 
-def telegram_bot(bot_token):
-    bot = telebot.TeleBot(bot_token)
+def infinity_polling(self, *args, **kwargs):
+    while not self.__stop_polling.is_set():
+        try:
+            self.polling(*args, **kwargs)
+        except Exception as e:
+            time.sleep(5)
+            pass
+    logger.info("Break infinity polling")
 
-    @bot.message_handler(commands=["start"])
-    def start_message(message):
-        bot.send_message(message.chat.id, "Привет! Пришли ID :)")
+bot = telebot.TeleBot(bot_token)
 
-    @bot.message_handler(content_types=["text"])
-    def send_text(message):
+@bot.message_handler(commands=["start"])
+def start_message(message):
+    bot.send_message(message.chat.id, "Привет!\nНачинаю считывать информацию.\nПо умолчанию интервал 90 минут.")
+    while True:
         try: 
-            while True:
-                vk_session = vk_api.VkApi(login, password)
-                vk_session.auth()
-                vk = vk_session.get_api()
+            vk_session = vk_api.VkApi(login, password)
+            vk_session.auth()
+            vk = vk_session.get_api()
 
-                GetFollowers = vk.users.getFollowers(user_id=f"{messageID}")
-                followers = GetFollowers['items']
+            GetFollowers = vk.users.getFollowers(user_id=f"{messageID}")
+            followers = GetFollowers['items']
 
-                GetFriends = vk.friends.get(user_id=f"{messageID}")
-                friends = GetFriends['items']
+            GetFriends = vk.friends.get(user_id=f"{messageID}")
+            friends = GetFriends['items']
 
-                time.sleep(10)
+            time.sleep(5400)
 
-                getUsers = vk.users.get(user_id=f"{messageID}", fields='last_seen,followers_count,photo_200_orig',)[0]
-                userID = getUsers['id']
-                userPhoto = getUsers['photo_200_orig']
-                fullname = getUsers['first_name'] + ' ' + getUsers['last_name']
-                lastseen = datetime.utcfromtimestamp(getUsers['last_seen']['time'] + 10800).strftime('%Y-%m-%d %H:%M')
+            getUsers = vk.users.get(user_id=f"{messageID}", fields='last_seen,followers_count,photo_200_orig',)[0]
+            userID = getUsers['id']
+            userPhoto = getUsers['photo_200_orig']
+            fullname = getUsers['first_name'] + ' ' + getUsers['last_name']
+            lastseen = datetime.utcfromtimestamp(getUsers['last_seen']['time'] + 10800).strftime('%Y-%m-%d %H:%M')
 
-                GetFollowers2 = vk.users.getFollowers(user_id=f"{messageID}")
-                followersCount = GetFollowers2['count']
-                NewFollower = list(set(GetFollowers2['items']) - set(GetFollowers['items']))
-                def newfollower():
-                    if NewFollower != []:
-                        NewFollowerGet = vk.users.get(user_id=f"{NewFollower[0]}")[0]
-                        NewFollowerName = NewFollowerGet['first_name'] + ' ' + NewFollowerGet['last_name']
-                        return NewFollowerName
-                    else:
-                        return "-"
+            GetFollowers2 = vk.users.getFollowers(user_id=f"{messageID}")
+            followersCount = GetFollowers2['count']
+            NewFollower = list(set(GetFollowers2['items']) - set(GetFollowers['items']))
+            def newfollower():
+                if NewFollower != []:
+                    NewFollowerGet = vk.users.get(user_id=f"{NewFollower[0]}")[0]
+                    NewFollowerName = NewFollowerGet['first_name'] + ' ' + NewFollowerGet['last_name']
+                    return NewFollowerName
+                else:
+                    return "-"
 
-                GetFriends2 = vk.friends.get(user_id=f"{messageID}")
-                friendsCount = GetFriends2['count']
-                NewFriend = list(set(GetFriends2['items']) - set(GetFriends['items']))
-                def newfriend():
-                    if NewFriend != []:
-                        NewFriendGet = vk.users.get(user_id=f"{NewFriend[0]}")[0]
-                        NewFriendName = NewFriendGet['first_name'] + ' ' + NewFriendGet['last_name']
-                        return NewFriendName
-                    else:
-                        return "-"
+            GetFriends2 = vk.friends.get(user_id=f"{messageID}")
+            friendsCount = GetFriends2['count']
+            NewFriend = list(set(GetFriends2['items']) - set(GetFriends['items']))
+            def newfriend():
+                if NewFriend != []:
+                    NewFriendGet = vk.users.get(user_id=f"{NewFriend[0]}")[0]
+                    NewFriendName = NewFriendGet['first_name'] + ' ' + NewFriendGet['last_name']
+                    return NewFriendName
+                else:
+                    return "-"
 
-                bot.send_message(message.chat.id, 
-f"ID пользователя: {userID}\nИмя пользователя: {fullname}\nПоследний раз в сети: {lastseen}\n\nОбщее кол-во друзей: {friendsCount}\nОбщее кол-во подписчиков: {followersCount}\n\nНовый подписчик: {newfollower()}\nНовый друг: {newfriend()}\n\n\n{userPhoto}") 
-        except Exception as ex: 
-            bot.send_message(message.chat.id, "Что-то пошло не так!")
+            bot.send_message(message.chat.id, 
+        f"ID пользователя: {userID}\nИмя пользователя: {fullname}\nПоследний раз в сети: {lastseen}\n\nОбщее кол-во друзей: {friendsCount}\nОбщее кол-во подписчиков: {followersCount}\n\nНовый подписчик: {newfollower()}\nНовый друг: {newfriend()}\n\n\n{userPhoto}")  
+        except Exception as _ex:
+            bot.send_message(message.chat.id, _ex)
 
-    bot.polling()
+bot.infinity_polling()
 
 def main():
-    telegram_bot(bot_token)
+    pass
     
 if __name__ == '__main__':
     main()
